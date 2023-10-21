f501dcd7a012d0ad6626063073606c3a0c935ec1
M1ngXU <max.mingxu@yahoo.com>
2023-10-21 13:35:38 +0200

    preparing to crawl mp4 urls

--- crawler/crawler.ts
+++ crawler/crawler.ts
@@ -44,7 +44,7 @@ type Lecture = { name: string, date: Date, lecturer: string, link: URL, other_id
 /// path should be the path of the url in the url box (without the base) or the whole url
 ///
 /// sends cookie if given, some lectures are 'protected'
-async function video_link_by_lecture_id(path: string, cookies?: string): Promise<Lecture> {
+async function get_video_link_by_lecture_id(path: string, cookies?: string): Promise<Lecture> {
     let id = '0'; //can be anything as long as not an empty string
     let path_prefix = path.split('/').slice(0, -1).join('/');
     let video_url = new URL(`${path_prefix}/${id}.series-metadata.json`, BASE_URL);
@@ -52,26 +52,21 @@ async function video_link_by_lecture_id(path: string, cookies?: string): Promise
     let json = await res.json();
 
     if (json['protection'] === 'PWD' || !json['authorized']) {
-        console.log('Not authorized: ' + JSON.stringify(json, null, 4));
+        // console.log('Not authorized: ' + JSON.stringify(json, null, 4));
         return null;
     }
+
 
     let selected = json['selectedEpisode'];
     let name = selected['title'];
     let lecturer = selected['createdBy'].join(', ');
     let date = new Date(selected['createdAt']);
-    let presentations: { height: number, width: number, type: string, url: string }[] = selected['media']['presentations'];
-    let links = presentations.filter(p => {
-        if (p.type === 'video/mp4') {
-            return true;
-        } else {
-            // also mp3?
-            console.log(p.type);
-            return false;
-        }
-    }).map(p => ({ resolution: p.width * p.height, link: new URL(p.url) }));
+    let presentations: { height: number, width: number, type: string, url: string }[] = selected['media']['presentations'] ?? selected['media']['presenters'];
+    let links = presentations.filter(p => p.type === 'video/mp4').map(p => ({ resolution: p.width * p.height, link: new URL(p.url) }));
     links.sort((v1, v2) => v1.resolution - v2.resolution);
     let other_ids = json['episodes'].map((e: { id: string }) => e.id);
+
+    if (links.length === 0) return null;
 
     return { name: name, lecturer: lecturer, date: date, link: links[0].link, other_ids: other_ids };
 }
@@ -113,16 +108,38 @@ async function get_lecture_links_by_query(query: string): Promise<string[]> {
 async function main() {
     console.time();
     // let links = await get_lecture_links_by_query('?');
-    let links = [
-        'https://video.ethz.ch/lectures/d-itet/2023/spring/227-0395-00L/dbbaf3d3-9fd7-4e34-9e5a-c6de79695cfc.html',
-        'https://video.ethz.ch/lectures/d-infk/2017/autumn/252-0027-00L/c5ede336-1102-4ff8-bcc4-ad1a294a5252.html',
-        'https://video.ethz.ch/lectures/d-math/2021/spring/401-1262-07L/551ccb9d-669e-4568-8d80-635a6a969ce5.html',
-        'https://video.ethz.ch/events/2019/rsl/83f9587c-82ea-43ab-94c4-ad2a78fc7dac.html'
-    ];
+    // let links = [
+    //     'https://video.ethz.ch/lectures/d-itet/2023/spring/227-0395-00L/dbbaf3d3-9fd7-4e34-9e5a-c6de79695cfc.html',
+    //     'https://video.ethz.ch/lectures/d-infk/2017/autumn/252-0027-00L/c5ede336-1102-4ff8-bcc4-ad1a294a5252.html',
+    //     'https://video.ethz.ch/lectures/d-math/2021/spring/401-1262-07L/551ccb9d-669e-4568-8d80-635a6a969ce5.html',
+    //     'https://video.ethz.ch/events/2019/rsl/83f9587c-82ea-43ab-94c4-ad2a78fc7dac.html'
+    // ];
+    let links = JSON.parse(fs.readFileSync('links.json'));
 
-    let videos = await Promise.all(links.map(async l => await video_link_by_lecture_id(l, await get_cookies(process.env.ETH_USERNAME, process.env.ETH_PASSWORD))));
+    let processes = [];
+    let video_links = [];
+    let cookies = await get_cookies(process.env.ETH_USERNAME, process.env.ETH_PASSWORD);
+
+    let left = links.length;
+    console.log('Total: ' + left);
+    while (left > 0) {
+        processes.push(get_video_link_by_lecture_id(links.at(-left), cookies));
+
+        left--;
+
+        if (processes.length == 500) {
+            let promises = await Promise.all(processes);
+            video_links.push(...promises);
+            processes.length = 0;
+
+            fs.writeFileSync('video-links.json', JSON.stringify(video_links));
+
+            console.log((links.length - left) + '/' + links.length);
+        }
+    }
+    // let videos = await Promise.all(links.map(async l => await video_link_by_lecture_id(l, await get_cookies(process.env.ETH_USERNAME, process.env.ETH_PASSWORD))));
     console.timeEnd();
-    fs.writeFileSync('video-links.json', JSON.stringify(videos, null, 4));
+    fs.writeFileSync('video-links.json', JSON.stringify(video_links, null, 4));
 }
 
 console.log('start');
