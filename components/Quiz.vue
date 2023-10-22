92f3cc65e8e8de7e7c68585873ddb35367049de2
ziad <135853660+ziadomalik@users.noreply.github.com>
2023-10-22 05:08:38 +0200

    feat: three tabs

---
+++ components/Quiz.vue
@@ -0,0 +1,12 @@
+<template>
+    <UButton block icon="i-heroicons-plus" size="xl">
+        <span class="">Generate questions based on the lecture</span>
+    </UButton>
+    <div class="opacity-50 gap-2 w-full flex flex-col items-center mt-10">
+        <h1 class="text-4xl">(ノಠ益ಠ)ノ彡┻━┻</h1>
+        <span>
+            No questions yet! 
+            <UButton variant="link" label="Get quizzed now!" size="sm" />
+        </span>
+    </div>
+</template>
